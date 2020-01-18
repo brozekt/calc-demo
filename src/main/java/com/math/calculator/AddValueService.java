ce767031f82f029b441b26f3758b06623ae6207c
Tomasz Brozek <tomaszbrozek@gmail.com>
2020-01-18 12:56:19 +0100

    fixed bug

--- src/main/java/com/math/calculator/AddValueService.java
+++ src/main/java/com/math/calculator/AddValueService.java
@@ -5,10 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AddValueService {
-    @Value("${additional.incrementor}")
-    private int additionalIncrementor;
-
     public int add(int x, int y) {
-        return x + y + 1;
+        return x + y;
     }
 }
